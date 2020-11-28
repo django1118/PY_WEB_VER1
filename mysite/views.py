842f8db2e98bebe8034e0d144303b0bf7c6262e8
lie-0022 <jayjun.rim@gmail.com>
2020-11-28 13:06:41 +0900

    w11 final

--- mysite/views.py
+++ mysite/views.py
@@ -7,9 +7,10 @@ from django.urls import reverse_lazy
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
 class UserCreateView(CreateView):
     template_name = "registration/register.html"
-    from_class = UserCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy("register_done")
 
 class UserCreateDoneTV(TemplateView):
